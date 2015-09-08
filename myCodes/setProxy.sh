8667d1592c1426f14885c1c0f85c82d8e1a10318
Denn Orina <anyoksdenn@gmail.com>
2015-09-08 15:21:46 +0300

    First commit Alot of testing and verification needed

---
+++ myCodes/setProxy.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+for i in http_proxy ftp_proxy https_proxy socks_proxy all_proxy HTTP_PROXY HTTPS_PROXY FTP_PROXY SOCKS_PROXY ALL_PROXY ; do export $i=http://P15%2F1253%2F2010%40students:uie%40uon@proxy.uonbi.ac.ke:80 ; done
+
